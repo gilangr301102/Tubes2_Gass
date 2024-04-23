9d53e99c72b018d4d99abb7c22a472c717a886ee
FOS-Intern <intern@fata-organa.com>
2024-04-24 01:09:04 +0700

    add scrapping wikipedia hyperlink (hardcode)

--- backend/hello.go
+++ backend/hello.go
@@ -8,31 +8,62 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
-func ExampleScrape() {
-	// Request the HTML page.
-	res, err := http.Get("http://metalsucks.net")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer res.Body.Close()
-	if res.StatusCode != 200 {
-		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
-	}
-
-	// Load the HTML document
-	doc, err := goquery.NewDocumentFromReader(res.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Find the review items
-	doc.Find(".left-content article .post-title").Each(func(i int, s *goquery.Selection) {
-		// For each item found, get the title
-		title := s.Find("a").Text()
-		fmt.Printf("Review %d: %s\n", i, title)
-	})
-}
-
 func main() {
-	ExampleScrape()
+	// Define the URL of the Wikipedia page
+	url := "https://en.wikipedia.org/wiki/indonesia"
+
+	// Define a user-agent header to simulate a browser request
+	headers := map[string]string{
+		"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+	}
+
+	// Create an HTTP client with custom headers
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for key, value := range headers {
+		req.Header.Set(key, value)
+	}
+
+	// Send an HTTP GET request to the URL with the headers
+	response, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+
+	print(response)
+
+	// Check if the request was successful (status code 200)
+	if response.StatusCode == 200 {
+		// Parse the HTML content of the page using goquery
+		doc, err := goquery.NewDocumentFromReader(response.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Initialize empty slice to store the table data
+		data := [][]string{}
+
+		// Iterate through the rows of the table
+		doc.Find("tr").Each(func(rowIdx int, row *goquery.Selection) {
+			// Skip the header row
+			if rowIdx == 0 {
+				return
+			}
+
+			// Extract data from each column and append it to the data slice
+			rowData := []string{}
+			row.Find("th,td").Each(func(colIdx int, col *goquery.Selection) {
+				rowData = append(rowData, col.Text())
+			})
+			data = append(data, rowData)
+		})
+
+		fmt.Print(data[0])
+	} else {
+		fmt.Println("Failed to retrieve the web page. Status code:", response.StatusCode)
+	}
 }
